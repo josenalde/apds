0ff0d23ff4af1c0c21a5d5768c97dcd178a96c72
josenaldeOliveira <josenalde@gmail.com>
2022-09-26 20:26:55 -0300

    Update Products.java

--- scripts/aula01/Products.java
+++ scripts/aula01/Products.java
@@ -1,4 +1,5 @@
 /*
+ COM FILE_4.java e PRODUCTS.CSV, incluir no pacote ENTITIES
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
