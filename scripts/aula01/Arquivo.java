0a63d98ddb35d976c2f1c5a15ad6a09c44f3f2ea
josenaldeOliveira <josenalde@gmail.com>
2022-09-26 21:02:59 -0300

    Add files via upload

---
+++ scripts/aula01/Arquivo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apds.file_1;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+/**
+ *
+ * @author josen
+ */
+public class Arquivo {
+    public static boolean Write(String path, String texto) {
+        try {
+            FileWriter fw = new FileWriter(path, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(texto);
+            //pw.flush();
+            bw.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+}
+
