d50db794a37789dbdbb36a082a223422515c00a1
josenaldeOliveira <josenalde@gmail.com>
2022-10-18 12:22:10 -0300

    Update ListFilesRecursively.java

--- scripts/aula05/ListFilesRecursively.java
+++ scripts/aula05/ListFilesRecursively.java
@@ -1,3 +1,6 @@
+// Percorre árvore de diretórios de gabarito de questões OBI, com estrutura de testes testX, inY1, inY2, outY1, outY2
+// Autor: Josenalde Oliveira out22
+
 package aula05;
 
 import java.io.File;
@@ -7,6 +10,8 @@ public class ListFilesRecursively {
 	public void listDirectoryTree(String startDir) {
 		File dir = new File(startDir);
 		File[] files = dir.listFiles();
+		
+		ManageIO extractValues = new ManageIO();
 
 		if (files != null && files.length > 0) {
 			for (File file : files) {
@@ -17,6 +22,8 @@ public class ListFilesRecursively {
 				} else {
 					// We can use .length() to get the file size
 					System.out.println(file.getName() + " (size in bytes: " + file.length()+")");
+					System.out.println(file.getAbsolutePath());
+					extractValues.ReadFile(file.getAbsolutePath());
 				}
 			}
 		}
