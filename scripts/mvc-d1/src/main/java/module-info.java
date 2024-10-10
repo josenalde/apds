02cf7cfbd469a959be75bced7dcaa05d693b3287
josenaldeOliveira <josenalde.oliveira@ufrn.br>
2024-10-09 22:50:19 -0300

    Update module-info.java

--- scripts/mvc-d1/src/main/java/module-info.java
+++ scripts/mvc-d1/src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module tecinfo.poo {
-    requires javafx.controls;
+    requires javafx.controls; //includes javafx.graphics
     requires javafx.fxml;
     requires java.sql;
-    requires javafx.graphics;
+    //requires javafx.graphics;
 
     opens tecinfo.poo.controller to javafx.fxml;
 
     exports tecinfo.poo;
-} 
+}
+
+//only works with -module <name> option during compilation
