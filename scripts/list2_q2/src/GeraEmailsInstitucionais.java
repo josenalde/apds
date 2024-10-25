acc3a7d419de5a208ad6e7a0bf8350f14a5e59a5
josenaldeOliveira <josenalde@gmail.com>
2024-10-24 21:37:43 -0300

    Add files via upload

--- scripts/list2_q2/src/GeraEmailsInstitucionais.java
+++ scripts/list2_q2/src/GeraEmailsInstitucionais.java
@@ -6,55 +6,57 @@ import java.util.Collections;
 
 public class GeraEmailsInstitucionais {
     public static void main(String[] args) throws Exception {
+        //obter o path completo clicando com o botão direito sobre o nome do arquivo docentes.csv e em copy path
         String path = "C:\\Users\\josen\\OneDrive\\ensino\\tads\\tad0009poo\\src\\list2_q2\\src\\docentes.csv";
-        String line;
-        String[] parsedLine = new String[]{null};
-        ArrayList<String> emailsInstitucionais =  new ArrayList<String>();
-        ArrayList<String> nomesDuplicados = new ArrayList<String>();
-        int d = 0;
+        String line; // para guardar temporariamente cada linha do arquivo
+        String[] parsedLine = new String[]{null}; // para guardar a linha já separada com cada parte em um vetor String
+        ArrayList<String> emailsInstitucionais =  new ArrayList<String>(); // para guardar os e-mails gerados
+        ArrayList<String> nomesDuplicados = new ArrayList<String>(); // apenas para guardar os nomes duplicados, só para conferência
+        
+        int d = 0; // apenas para guardar quantos duplicados encontrou (debug)
         //ideia de instanciar diretamente no try, portanto a memoria é liberada quando o escopo do try encerrar
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) { //neste caso não precisa do finally
-            //ler linha a linha
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) { //Classes FileReader e BufferedReader trabalham sempre JUNTAS
+            //ler linha do cabeçalho (primeira linha) só para descartar e pular a linha
             line = br.readLine(); //cabeçalho (descartar)
-            //System.out.println(line);
-            line = br.readLine();
-            //System.out.println(line);
-            while (line != null) { //equivale ao EOF do Scanner
-                parsedLine = line.split(";");
-                if (emailsInstitucionais.contains(generateEmail(parsedLine[1],false))) {
+            //System.out.println(line); // se printar, irá aparecer o cabeçalho com os nomes de cada coluna do banco de dados
+            line = br.readLine(); // lê a segunda linha do banco de dados, já com o primeiro docente
+            
+            while (line != null) { //equivale ao EOF do Scanner, ou seja, lê até o fim do arquivo, quando não há mais linhas para ler retorna NULL
+                parsedLine = line.split(";"); // a linha separada por ; é armazenada no vetor parsedLine, logo, o nome é o segundo dado, de índice 1
+                if (emailsInstitucionais.contains(generateEmail(parsedLine[1],false))) { // uso do contains para checar se já existe
                     //System.out.println("EMAIL DUPLICADO");
                     d+=1;
-                    emailsInstitucionais.add(generateEmail(parsedLine[1],true));
-                    nomesDuplicados.add(parsedLine[1]);
+                    emailsInstitucionais.add(generateEmail(parsedLine[1],true)); // se já tem, adiciona segundo nome também. É tratado na função generateEmail
+                    nomesDuplicados.add(parsedLine[1]); // só para debug
                 } else {
-                    emailsInstitucionais.add(generateEmail(parsedLine[1],false));
+                    emailsInstitucionais.add(generateEmail(parsedLine[1],false)); // se é a primeira vez, o segundo parâmetro é FALSE
                 }
-                line = br.readLine();
+                line = br.readLine(); // lê próxima linha e volta o loop
             }
-            Collections.sort(nomesDuplicados);
-            Collections.sort(emailsInstitucionais);
-            System.out.println(d + "nomes duplicados ");
+            //Collections.sort(nomesDuplicados); // ordena só para fins de exibição na tela
+            //System.out.println(d + "nomes duplicados ");
             /*
             for (String n : nomesDuplicados)  {
                 System.out.println(n);
             }*/
+            Collections.sort(emailsInstitucionais); // ordena só para fins de exibição na tela
             for (String e : emailsInstitucionais)  {
-                 System.out.println(e);
+                 System.out.println(e); // itera a lista e exibe os e-mails gerados
             }
         } catch(IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage()); // caso dê qualquer erro no processo, avisa aqui...
         }
     }
-    private static String generateEmail(String email, boolean isDuplicated) {
+    // criada por Josenalde Oliveira. Recebe o nome docente e se o nome é duplicado ou não
+    private static String generateEmail(String name, boolean isDuplicated) {
         String first, last, second;
-        //System.out.println(email);
-        first = email.split(" ")[0];
-        last = email.split(" ")[email.split(" ").length-1];
-        if (!isDuplicated) {
-            return (first + "." + last).toLowerCase();
+        first = name.split(" ")[0];
+        last = name.split(" ")[name.split(" ").length-1]; // para obter o último índice do vetor de partes do nome
+        if (!isDuplicated) { // se não é duplicado, o nome é first.last@ufrn.br
+            return (first + "." + last).toLowerCase() + "@ufrn.br";
         } else {
-            second = email.split(" ")[1];
-            return (first + "." + second + "." + last).toLowerCase();
+            second = name.split(" ")[1];
+            return (first + "." + second + "." + last).toLowerCase() + "@ufrn.br";
         }
     }
 }
