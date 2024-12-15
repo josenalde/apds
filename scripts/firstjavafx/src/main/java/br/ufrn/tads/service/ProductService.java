509faca971f23de8d14259ae66a3c10d4dc95049
josenaldeOliveira <josenalde.oliveira@ufrn.br>
2024-12-15 12:10:40 -0300

    Update ProductService.java

--- scripts/firstjavafx/src/main/java/br/ufrn/tads/service/ProductService.java
+++ scripts/firstjavafx/src/main/java/br/ufrn/tads/service/ProductService.java
@@ -3,18 +3,12 @@ package br.ufrn.tads.service;
 import java.util.List;
 
 import br.ufrn.tads.model.Product;
-//import br.ufrn.tads.repository.DBconnection;
 import br.ufrn.tads.repository.ProductDao;
 
 public class ProductService {
     
     private ProductDao productDao;
     
-    /*public boolean testConnection() {
-        if (DBconnection.getConnection() != null) return true;
-        else return false;
-    }*/
-
     public List<Product> getProducts() {
         productDao = new ProductDao();
         return productDao.findAll();
